22a4104829d78d59aa97eb1dc3b6fe620dfe61b0
Aline Moura <alinesouzamoura_@hotmail.com>
2023-01-06 15:20:31 -0300

    routes working

--- src/app.js
+++ src/app.js
@@ -3,25 +3,46 @@ import cors from "cors";
 
 const server = express();
 
+server.use(express.json());
 server.use(cors());
 
-server.get("/receitas", (req, responda) => {
-  const receitas = [
-    {
-      id: 1,
-      titulo: "Pão com Ovo",
-      ingredientes: "Ovo e pão",
-      preparo: "Frite o ovo e coloque no pão",
-    },
-    {
-      id: 2,
-      titulo: "Mingau de Whey",
-      ingredientes: "Leite, Aveia e Whey",
-      preparo: "Mistura tudo na panela fervendo",
-    },
-  ];
+const usersData = [];
+const tweetsData = [];
 
-  responda.send(receitas);
+function findAvatar(tweet) {
+  const { avatar } = usersData.find((item) => item.username === tweet.username);
+  return avatar;
+}
+
+server.get("/tweets", (req, res) => {
+  const updatedTweets = tweetsData.map((tweet) => {
+    const avatar = findAvatar(tweet);
+    return {
+      username: tweet.username,
+      avatar: avatar,
+      tweet: tweet.tweet,
+    };
+  });
+
+  const tweetsCopy = [...updatedTweets].reverse();
+  const fetchedTweets = tweetsCopy.slice(0, 10);
+
+  res.send(fetchedTweets);
+});
+
+server.post("/sign-up", (req, res) => {
+  const user = req.body;
+  console.log("user", user);
+
+  usersData.push(user);
+  res.status(201).send("OK");
+});
+
+server.post("/tweets", (req, res) => {
+  const tweet = req.body;
+  tweetsData.push(tweet);
+
+  res.status(201).send("OK");
 });
 
 server.listen(5000, () => {
